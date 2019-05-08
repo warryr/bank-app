59ca4040118e4f651a0105b4e7f980625dff2a82
warryr <heartsyrop@gmail.com>
2019-05-08 13:48:49 +0300

    feat: add header for all pages except login

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import AllClients from './components/client/AllClients'
+import AllClients from './components/client/AllClients';
 import SingleClient from './components/client/SingleClient';
 import DepositForm from './components/deposit/DepositForm';
 import CreditForm from './components/credit/CreditForm';
 import LoginWrapper from './components/login/LoginWrapper';
+import Header from './components/common/header';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
+          <Switch>
+            <Route path='/login' exact={true}/>
+            <Route path='/' component={Header}/>
+          </Switch>
           <Route path='/login' exact={true} component={LoginWrapper}/>
           <Route path='/clients' exact={true} component={AllClients}/>
           <Route path='/clients/:id([0-9a-fA-f]{24})' exact={true} component={SingleClient}/>
